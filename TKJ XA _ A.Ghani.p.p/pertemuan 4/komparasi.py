f83f146cdc1da77ee1143e8b5410bcfbe7238462
inlineos <104988199+g-internet@users.noreply.github.com>
2024-08-01 12:12:01 +0700

    Add files via upload

---
+++ TKJ XA _ A.Ghani.p.p/pertemuan 4/komparasi.py
@@ -0,0 +1,20 @@
+a = 10 
+b = 11
+c = 12
+d = 13
+e = 14
+f = 15
+g = 16
+h = 1
+i = 12
+j = 12
+k = 23
+l = 90
+ 
+
+print(a > b)
+print(c < d)
+print(e == f)
+print(g >= h)
+print(i <= j)
+print(k != l)
---
+++ TKJ XA _ A.Ghani.p.p/pertemuan 4/penugasan.py
@@ -0,0 +1,27 @@
+# variabel
+a = 10 
+b = 11
+c = 12
+d = 13
+e = 14
+f = 15
+g = 16
+h = 17
+
+a += 1
+print("+",a)
+
+b -= 2
+print("-",b)
+
+c *= 3
+print("*",c)
+
+d /= 4
+print("/",d)
+
+e **= 5
+print("**",e)
+
+
+
