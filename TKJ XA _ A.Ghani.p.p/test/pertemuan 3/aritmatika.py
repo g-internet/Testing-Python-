95d3e549c012c24528afc27dceb819b6e5821900
inlineos <104988199+g-internet@users.noreply.github.com>
2024-08-13 08:46:50 +0700

    Add files via upload

---
+++ TKJ XA _ A.Ghani.p.p/test/pertemuan 3/aritmatika.py
@@ -0,0 +1,10 @@
+a = 9
+b = 8
+
+print("hasil :", a + b)
+print("hasil :", a - b)
+print("hasil :", a * b)
+print("hasil :", a / b)
+print("hasil :", a // b)
+print("hasil :", a % b)
+print("hasil :", a ** b)
