c318139532f82366c466109216c54cbb39215658
inlineos <104988199+g-internet@users.noreply.github.com>
2024-09-19 11:56:52 +0700

    Update 24-09-19.py

--- belajar/exam/24-09-19.py
+++ belajar/exam/24-09-19.py
@@ -1,17 +1,20 @@
-import time
-print("selamat datang di bisokop")
+print("Selamat datang di bioskop")
 
 harga_tiket_anak = 30000
 harga_tiket_dewasa = 50000
 harga_tiket_lansia = 35000
 
-penonton = int(input("jumlah penonton: "))
-
+penonton = int(input("Jumlah penonton: "))
 
 for i in range(penonton):
-    tiket_anak = int(input("masukan jumlah tiket anak: "))
-    tiket_dewasa = int(input("masukan jumlah tiket dewasa: "))
-    tiket_lansia = int(input("masukan jumlah tiket lansia: "))
+    print(f"\nPenonton Ke {i+1}:")
+    tiket_anak = int(input("Masukkan jumlah tiket anak: "))
+    tiket_dewasa = int(input("Masukkan jumlah tiket dewasa: "))
+    tiket_lansia = int(input("Masukkan jumlah tiket lansia: "))
+
     total_harga_tiket = (tiket_anak * harga_tiket_anak) + (tiket_dewasa * harga_tiket_dewasa) + (tiket_lansia * harga_tiket_lansia)
-    print(f"kamu membeli tiket anak sebanyak {tiket_anak} dengan harga {harga_tiket_anak} , tiket dewasa sebanyak {tiket_dewasa} dengan harga {harga_tiket_dewasa}, tiket lansia sebanyak {tiket_lansia} dengan harga {harga_tiket_lansia}")
-    print(f"total kamu membeli itu {total_harga_tiket}")
+
+    print(f"Kamu membeli tiket anak sebanyak {tiket_anak} dengan harga {harga_tiket_anak} per tiket,")
+    print(f"tiket dewasa sebanyak {tiket_dewasa} dengan harga {harga_tiket_dewasa} per tiket,")
+    print(f"tiket lansia sebanyak {tiket_lansia} dengan harga {harga_tiket_lansia} per tiket.")
+    print(f"Total harga tiket yang kamu beli adalah Rp{total_harga_tiket}\n")
